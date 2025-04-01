e7c1372276f3dcfcfb23fdce8ade728520222f34
capthndsme <nieoy@msn.com>
2025-04-01 13:18:17 +0800

    Sync

--- app/service/GPIOService.ts
+++ app/service/GPIOService.ts
@@ -7,7 +7,31 @@ class GPIOService {
 
   booted: boolean = false;
   inputBlocking: boolean = false;
+  magnetState: boolean = false;
   readonly isPi: boolean = env.get('IS_PI');
+
+  /** 
+   *  || This lists a callback when magnetic state changes
+   * 
+   */
+  
+  
+  #magnetStateCallbacks: ((state: boolean) => void)[] = [];
+
+  registerMagnetStateCallback(callback: (state: boolean) => void): void {
+    this.#magnetStateCallbacks.push(callback);
+  }
+
+  unregisterMagnetStateCallback(callback: (state: boolean) => void): void {
+    this.#magnetStateCallbacks = this.#magnetStateCallbacks.filter(cb => cb !== callback);
+  }
+
+  notifyMagnetStateCallbacks(state: boolean): void {
+    this.#magnetStateCallbacks.forEach(callback => callback(state));
+  }
+  
+
+
 
   public boot(): void {
     if (this.booted) return console.log("GPIO service already booted");
@@ -20,33 +44,19 @@ class GPIOService {
 
     // Initialize MagnetLock detection
     gpiox.init_gpio(this.map.output.SolenoidLock, gpiox.GPIO_MODE_OUTPUT, false)
-
-
-
-   /*  // Init LED output
-    const leds = Object.keys(GPIOMap.LEDS);
-    leds.forEach(led => {
-      gpiox.init_gpio(GPIOMap.LEDS[led as keyof typeof GPIOMap['LEDS']], gpiox.GPIO_MODE_OUTPUT, false);
-    });
-
-
-    // init led ringlight
-
-    gpiox.init_gpio(GPIOMap.ringLight, gpiox.GPIO_MODE_OUTPUT, false);
-
-
-
-
-    // Init door
-    gpiox.init_gpio(GPIOMap.door, gpiox.GPIO_MODE_OUTPUT, true);
-
-    // Polling for button press with debouncing
-  
-    gpiox.watch_gpio(GPIOMap.BUTTONS.openBtn, gpiox.GPIO_MODE_INPUT_PULLUP, 4000, gpiox.GPIO_EDGE_FALLING, (state, edge, pin) => this.pollOpenBtn(pin)); */
-
-    // @ts-ignore - ABYSMAL typings for gpiox. 
-    gpiox.watch_gpio(this.map.input.MagnetDetection, gpiox.GPIO_MODE_INPUT_PULLUP, 4000, gpiox.GPIO_EDGE_FALLING, (state, edge, pin) => this.pollStartBtn(pin));
-
+   
+    // 
+    // create timeout loop
+    setInterval(() => {
+      const state = gpiox.get_gpio(this.map.input.MagnetDetection);
+      if (state !== this.magnetState) {
+        this.magnetState = state;
+        console.log(`Magnet state changed to ${state ? 'detected' : 'not detected'}`);
+        this.notifyMagnetStateCallbacks(state);
+      }
+    }, 100);
+    
+    
   }
 
   writeLockState(state: boolean): void {
@@ -56,14 +66,7 @@ class GPIOService {
 
 
 
-
-
-  private pollStartBtn(pin: number): void {
  
-    console.log("detected a magnet!", pin);
-
-
-  }
   map = {
     output: {
       SolenoidLock: 17,
